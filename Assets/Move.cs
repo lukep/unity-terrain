2e96a8f0bf1d660a2a030175c5697fb365610952
Luke Parrott <lparrott@modernclimate.com>
2015-11-08 16:19:05 -0600

    Good stuff

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 
 public class Move : MonoBehaviour {
+	public Camera cam;
 	public float speed = 6.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
+	private float mouseX = 0;
 	private Vector3 moveDirection = Vector3.zero;
 	void Update() {
 		CharacterController controller = GetComponent<CharacterController>();
+
 		if (controller.isGrounded) {
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection);
@@ -15,6 +18,16 @@ public class Move : MonoBehaviour {
 			if (Input.GetButton("Jump")){
 				moveDirection.y = jumpSpeed;
 			}
+
+			Vector3 point = cam.ScreenToViewportPoint(Input.mousePosition);
+			mouseX = (float)(point.x - 0.5); // Input.GetAxis("Mouse X");
+			//Debug.Log (mouseX);
+
+			if( Mathf.Abs(mouseX) > 0.2 ){
+				transform.Rotate(Vector3.up * mouseX * speed/30);
+				//transform.Rotate(Vector3.up * mouseX);
+			}
+
 		}
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
--- Assets/TerrainManager.cs
+++ Assets/TerrainManager.cs
@@ -24,7 +24,7 @@ public class TerrainManager : MonoBehaviour {
 	private int tileWidth;
 	private int tileHeight;
 
-	private enum Directions {Front, Right, Back, Left};
+	private enum Directions {Front, Right, Back, Left, None};
 	
 	// Use this for initialization
 	void Start () {
@@ -60,25 +60,27 @@ public class TerrainManager : MonoBehaviour {
 		string dbgString = "";
 		dbgString = "CurrentID : " + currentTerrainID[0] + ", " + currentTerrainID[1] + "\n\n";
 
-
-
-
-		// Starts behind the player?
+		// We always draw the initial tile first and grow from there
+		DropTerrainAt(0, 0);
 
 		// Spread is the amount of surrounding terrains to draw
 
 		// Why is this called so often?
 
-		for(int i=-spread;i<=spread;i++)
-		{
-			for(int j=-spread;j<=spread;j++)
-			{	
+		for(int i=-spread;i<=spread;i++){
+			for(int j=-spread;j<=spread;j++){
+
+				if(i == 0 && j == 0){
+				//	break;
+				}
+
 				DropTerrainAt(currentTerrainID[0] + i, currentTerrainID[1] + j);
 				dbgString += (currentTerrainID[0] + i) + "," + (currentTerrainID[1] + j) + "\n";
+
 			}
 		}
 
-		//Debug.Log(dbgString);
+		Debug.Log(dbgString);
 
 		// TODO: Reactivate this when needed
 		//ReclaimTiles();
@@ -94,25 +96,21 @@ public class TerrainManager : MonoBehaviour {
 	{
 		bool doUpdate = false;
 		// Check if terrain exists, if it does, activate it.
-		if(terrainUsage.ContainsKey(i, j) && terrainUsage[i,j] != -1)
-		{
+		if(terrainUsage.ContainsKey(i, j) && terrainUsage[i,j] != -1){
 			// Tile mapped, use it.
 		}
 		// If terrain doesn't exist, drop it.
-		else
-		{
+		else{
 			doUpdate = true;
 
 			terrainUsage[i,j] = FindNextAvailableTerrainID();
 			if(terrainUsage[i,j] == -1) Debug.LogError("No more tiles, failing...");
 		}
 
-		if(terrainUsageData.ContainsKey(i,j))
-		{
+		if(terrainUsageData.ContainsKey(i,j)){
 			// Restore the data for this tile
 		}
-		else
-		{
+		else{
 			doUpdate = true;
 
 			// Create a new data object
@@ -133,8 +131,7 @@ public class TerrainManager : MonoBehaviour {
 
 	}
 	
-	TerrainData CreateNewTerrainData()
-	{
+	TerrainData CreateNewTerrainData(){
 
 		TerrainData tData = new TerrainData();
 		CopyTerrainDataFromTo(referenceTerrain.terrainData, ref tData);
@@ -142,13 +139,11 @@ public class TerrainManager : MonoBehaviour {
 
 	}
 	
-	void ResetTouch()
-	{
+	void ResetTouch(){
 		touchedTiles.SetAll(false);
 	}
 	
-	int CountOnes(BitArray arr)
-	{
+	int CountOnes(BitArray arr){
 		int count = 0;
 		for(int i=0;i<arr.Length;i++)
 		{
@@ -158,8 +153,7 @@ public class TerrainManager : MonoBehaviour {
 		return count;
 	}
 	
-	void ReclaimTiles()
-	{
+	void ReclaimTiles(){
 		if(CountOnes(usedTiles) > ((spread*2 + 1)*(spread*2 + 1)))
 		{
 			for(int i=0;i<usedTiles.Length;i++)
@@ -173,8 +167,7 @@ public class TerrainManager : MonoBehaviour {
 		}
 	}
 	
-	void ActivateUsedTile(int i, int j)
-	{
+	void ActivateUsedTile(int i, int j){
 		terrainBuffer[terrainUsage[i, j]].gameObject.transform.position = 
 									new Vector3(  	referencePosition.x + i * referenceSize.x,
 													referencePosition.y,
@@ -194,6 +187,10 @@ public class TerrainManager : MonoBehaviour {
 
 	}
 
+	bool TileExists(int i, int j){
+		return (terrainUsage.ContainsKey(i, j) && terrainUsage[i,j] != -1);
+	}
+
 	void UpdateTerrain(int i, int j){
 
 		Terrain terrainBlock = terrainBuffer [terrainUsage [i, j]];
@@ -205,18 +202,9 @@ public class TerrainManager : MonoBehaviour {
 
 		int xResolution = terrainBlock.terrainData.heightmapWidth;
 		int zResolution = terrainBlock.terrainData.heightmapHeight;
-		
-		Vector2 terrainSize = new Vector2 (xResolution, zResolution);
-		float heightVal = 0;
-		float xFactor, yFactor;
-		int midPoint = (int)terrainSize.x / 2;
 
-		float heightFactor = 600.0f;
-		
-		float[,] heights = new float[ (int)terrainSize.x, (int)terrainSize.y ];  //terrainBlock.terrainData.GetHeights(0, 0, (int)terrainSize.x, (int)terrainSize.y);
-
-		//float[,] thisStartHeight = new float[,]{{0}};
-		float[] thisStartHeight = new float[xResolution];
+		float[][] borders = new float[4][]{ new float[]{0},new float[]{0},new float[]{0},new float[]{0}};
+	
 
 
 		/*
@@ -227,60 +215,124 @@ public class TerrainManager : MonoBehaviour {
 		*/
 
 
-		// Look around this tile to alter its heightmap
+		// Should call on ALL border tiles to get the array of border values, which are used to create the new terrain
 
 
 
-		// Test with just the terrain in FRONT of the player
-		if(i == currentTerrainID[0] && j == currentTerrainID[1] ){
-
-			thisStartHeight = ReturnBorder(i, j-1, Directions.Back);
-			//Debug.Log("START HEIGHT: " + i + " " + j + " " + thisStartHeight[70] + " " + thisStartHeight[80]);
-
-		} else if (i == currentTerrainID[0]+1 && j == currentTerrainID[1]-1){
-
-			// TODO: Alter the tile to the right!
-
-			//thisStartHeight = ReturnBorder(i+1, j, Directions.Right);
-
+		//if(i == currentTerrainID[0] && j == currentTerrainID[1] ){
+		if (TileExists(i, j-1)){
+			// Call on the tile behind this one
+			borders[2] = ReturnBorder(i, j-1, Directions.Back);
+		} 
+		if (TileExists(i, j+1)){
+			// Call on the tile to the front of this one
+			borders[0] = ReturnBorder(i, j+1, Directions.Front);
+		}
+		if (TileExists(i+1, j)){
+			// Call on the tile to the right of this one
+			borders[1] = ReturnBorder(i+1, j, Directions.Right);
+		} 
+		if (TileExists(i-1, j)){
+			// Call on the tile to the left of this one
+			borders[3] = ReturnBorder(i-1, j, Directions.Left);
 		}
 
-		// Loop over every point on the terrain and adjust the height
-		for (int My = 0; My < terrainSize.y; My++) {
-
-			// Set the Starting X and Y values to the same as the neighboring values
-
-			for (int Mx = 0; Mx < terrainSize.x; Mx++) {
-
-				if(Mx < midPoint){
-					xFactor = Mx/2 + (thisStartHeight[My] * heightFactor);
-				} else {
-					xFactor = Mx/2 + (thisStartHeight[My] * heightFactor);
-					//xFactor = midPoint - (Mx - midPoint) + frontHeights[0,0];
-				}
-
-				if(My < midPoint)
-					yFactor = My + (thisStartHeight[My] * heightFactor);
-				else
-					yFactor = My + (thisStartHeight[My] * heightFactor);  //midPoint - (My - midPoint);
-
-				heightVal = xFactor; // + yFactor;
+		// Loop over every point on the terrain and adjust the height accordingly
+		terrainBlock.terrainData.SetHeights(0, 0, ReturnHeightmap(borders, xResolution, zResolution));
 
 
-				heights[Mx, My] = (float)((float)heightVal / heightFactor);
+		// Also update the collider
+		TerrainCollider tc = terrainBlock.gameObject.GetComponent<TerrainCollider>();
+		tc.terrainData = terrainBlock.terrainData;
+
+	}
+
+	float[,] ReturnHeightmap(float[][] borders, int x, int y){
+
+
+		float xFactor, yFactor;
+		float borderChange = 0;
+		float heightFactor = 600.0f;
+		float heightVal = 0;
+		float[,] heights = new float[ (int)x, (int)y ];
+		bool[] hasBorder = {false,false,false,false};
+		float[] randHeights = new float[4];
+		float[] borderHeights = new float[4];
+
+	
+
+		// Given the borders, fill in the rest of the height map accordingly
+
+
+
+		// TODO: Figure out how to add some randomness while still keeping borders correct
+
+
+
+		// Need to define them here so they stay the same!
+
+		if(borders[0].Length > 1)
+			hasBorder[0] = true;
+		else
+			randHeights[0] = Random.Range(0.0f, 0.2f);
+
+		if(borders[1].Length > 1)
+			hasBorder[1] = true;
+		else
+			randHeights[1] = Random.Range(0.0f, 0.2f);
+
+		if(borders[2].Length > 1)
+			hasBorder[2] = true;
+		else
+			randHeights[2] = Random.Range(0.0f, 0.2f);
+
+		if(borders[3].Length > 1)
+			hasBorder[3] = true;
+		else
+			randHeights[3] = Random.Range(0.0f, 0.2f);
+
+
+		// Moving from left to right
+		for (int My = 0; My < y; My++) {
+
+
+			// Moving from back to front
+			for (int Mx = 0; Mx < x; Mx++) {
+				
+				borderChange = 0;
+				xFactor = 0;
+				yFactor = 0;
+
+
+				borderHeights[0] = hasBorder[0] ? borders[0][My] : randHeights[0];
+				borderHeights[1] = hasBorder[1] ? borders[1][Mx] : randHeights[1];
+				borderHeights[2] = hasBorder[2] ? borders[2][My] : randHeights[2];
+				borderHeights[3] = hasBorder[3] ? borders[3][Mx] : randHeights[3];
+
+				// Bottom, top, etc means I'm GETTING my border from that direction
+
+				//(float)(My/y)
+
+				// Top and bottom
+				xFactor = Mathf.Lerp( borderHeights[2], borderHeights[0], (float)Mx/x );
+
+				// Left and right borders
+				yFactor = Mathf.Lerp( borderHeights[3], borderHeights[1], (float)My/y );
+
+
+				//xFactor += (borderChange * heightFactor) + (x - Mx);
+				
+				heightVal = xFactor + yFactor;
+				
+				heights[Mx, My] = (float)((float)heightVal);
+
+
 
 			}
 
 		}
 
-		terrainBlock.terrainData.SetHeights(0, 0, heights);
-
-
-		// Also update the collider?
-		TerrainCollider tc = terrainBlock.gameObject.GetComponent<TerrainCollider>();
-		tc.terrainData = terrainBlock.terrainData;
-
-		//terrainBlock.terrainData.RefreshPrototypes();
+		return heights;
 
 	}
 
@@ -294,10 +346,19 @@ public class TerrainManager : MonoBehaviour {
 
 		if(dir == Directions.Back){
 			for (int z = 0; z < tileWidth; z++){
-				retBorder.Add(heights[tileHeight-1, z]);
+				retBorder.Add(heights[tileWidth-1, z]);
 			}
-		} else if(dir == Directions.Right){
-			for (int x = 0; x < tileWidth; x++){
+		} else if(dir == Directions.Front){
+			for (int z = 0; z < tileWidth; z++){
+				retBorder.Add(heights[0, z]);
+			}
+		}
+		else if(dir == Directions.Right){
+			for (int x = 0; x < tileHeight; x++){
+				retBorder.Add(heights[x, 0]);
+			}
+		} else if(dir == Directions.Left){
+			for (int x = 0; x < tileHeight; x++){
 				retBorder.Add(heights[x, tileHeight-1]);
 			}
 		}
